231eb2a5268a3d8c37c703e909b9f1b344fd5452
kadir mert yildiz <kadirmert.34@hotmail.com>
2024-10-28 20:54:09 +1100

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Kadir Yildiz on 28/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
